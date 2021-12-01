c9e9e146a2fb0d4e1a9fe26259881e04aab46173
suyeon-hong <bellahong95@gmail.com>
2021-12-01 14:27:58 +0900

    join페이지 LetterMotion JS class로 수정

--- js/join.js
+++ js/join.js
@@ -1,26 +1,35 @@
-letter(".join h1", 0.1);
+class LetterMotion{
+    constructor(){
+        this.letter(".join h1", 0.1);
+    }
+    letter(item, interval){
+        const frame = document.querySelector(item);
+        let txts = frame.innerText;
+        let num = 0;
+    
+        txts = txts.split(" ");
+        frame.innerText = "";
+    
+        txts.forEach((txt,index)=>{
+            const htmls = `<span>${txt}</span>`;
 
-function letter(item, interval){
-    let txt = $(item).text();
-    let num = 0;
+            frame.innerHTML += htmls;
+            frame.querySelectorAll("span")[index].style.transitionDelay = `${num*interval}s`;
+            frame.querySelectorAll("span")[index].style.transform = "translateY(-200px)";
+            frame.querySelectorAll("span")[index].style.opacity = 0;
 
-    txt = txt.split(" ");
-    $(item).empty();
+            num++;
 
-    for(let el of txt){
-        $(item).append(
-            $("<span>").text(el).css({
-                transitionDelay: num*interval +"s",
-                transform: "translateY(-200px)",
-                opacity: 0
-            })
-        )
-        num++;
-
-        $(item).find("span").stop().animate({
-            opacity: 1
-        }, 500, function(){
-            $(this).css({transform: "translateY(0)"})
+            for(let el of frame.querySelectorAll("span")){
+                new Anim(el,{
+                    prop: "opacity",
+                    value: 1,
+                    duration: 500,
+                    callback: ()=>{
+                        el.style.transform = "translateY(0)"
+                    }
+                });
+            }
         });
     }
 }
