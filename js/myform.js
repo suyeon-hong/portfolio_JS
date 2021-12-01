09501e4c46d3c162bf9270f9b3b9737f61f4556e
suyeon-hong <bellahong95@gmail.com>
2021-12-01 13:59:56 +0900

    join페이지 form validation JS 수정 (isEmail 오류있음)

--- js/myform.js
+++ js/myform.js
@@ -4,13 +4,14 @@ class MyForm{
         this.eventBinding(option);
     }
     init(selector){
-        this.submitBtn = $(selector);
+        this.frame = document.querySelector(selector);
+        this.submitBtn = this.frame.querySelector("input[type=submit]");
         this.result = [];
     }
     
     eventBinding(option){
         option.forEach((opt)=>{
-            this.submitBtn.on("click", e=>{
+            this.submitBtn.addEventListener("click", e=>{
                 
                 if(opt.type == "id"){
                     if(!this.isId(opt.name)) e.preventDefault();
@@ -38,188 +39,249 @@ class MyForm{
                 }
             });
         });
-        this.submitBtn.on("click", e=>{
+        this.submitBtn.addEventListener("click", e=>{
+            e.preventDefault();
+
             if(this.result.length) {
-                $(window).scrollTop(0);
-                alert("아래 항목의 필수 입력값을 확인해 주세요\n\n" + this.result);
+                window.scroll = 0;
+                alert(`아래 항목의 필수 입력값을 확인해 주세요\n\n ${this.result}`);
                 this.result = [];
             }
         });
     }
     
     isId(name){
-        let txt = $("[name="+ name +"]").val();
-        let eng = /[a-z]/;
-        let num = /[0-9]/;
+        const txt = document.querySelector(`[name= ${name}]`);
+        const val = txt.value;
+        const eng = /[a-z]/;
+        const num = /[0-9]/;
     
-        if(txt.length >= 4 && txt.length <= 16 && eng.test(txt) || num.test(txt)){
-            $("[name="+ name +"]").parent().find("p").remove();
+        if(txt.length >= 4 && txt.length <= 16 && eng.test(val) || num.test(val)){
+            const errMsg = txt.closest("td").querySelector("p");
+            if(errMsg) errMsg.remove();
             return true;
         }else if(txt.length >= 4 && txt.length <= 16){
-            $("[name="+ name +"]").parent().find("p").remove();
-            $("[name="+ name +"]").parent().append(
-                "<p class='caution'>영문소문자/숫자를 포함한 4~16자 이내로 입력해 주세요.</p>"
-            );
-            this.result.push($("label[for="+ name +"]").text());
+            const errMsg = txt.closest("td").querySelector("p");
+            if(errMsg) errMsg.remove();
+
+            const htmls = "<p class='caution'>영문소문자/숫자를 포함한 4~16자 이내로 입력해 주세요.</p>";
+            txt.closest("td").innerHTML += htmls;
+
+            this.result.push(document.querySelector(`label[for= ${name}]`).innerText);
             return false;
         }else{
-            $("[name="+ name +"]").parent().find("p").remove();
-            $("[name="+ name +"]").parent().append(
-                "<p class='caution'>아이디를 입력해 주세요.</p>"
-            );
-            this.result.push($("label[for="+ name +"]").text());
+            const errMsg = txt.closest("td").querySelector("p");
+            if(errMsg) errMsg.remove();
+
+            const htmls = "<p class='caution'>아이디를 입력해 주세요.</p>";
+            txt.closest("td").innerHTML += htmls;
+
+            this.result.push(document.querySelector(`label[for= ${name}]`).innerText);
             return false;
         }
     }
     
     isPw(name1, name2){
-        let txt1 = $("[name = "+ name1 +"]").val();
-        let txt2 = $("[name = "+ name2 +"]").val();
-        let eng = /[a-zA-Z]/;
-        let num = /[0-9]/;
-        let spc = /[~!@#$%^&*()_+]/;
+        const txt1 = document.querySelector(`[name= ${name1}]`);
+        const txt2 = document.querySelector(`[name= ${name2}]`);
+        const val1 = txt1.value;
+        const val2 = txt2.value;
+        const eng = /[a-zA-Z]/;
+        const num = /[0-9]/;
+        const spc = /[~!@#$%^&*()_+]/;
     
-        if(txt1 === txt2 && txt1.length >= 8 && txt1.length <= 16){
-            if(eng.test(txt1) && num.test(txt1)){
-                $("[name="+ name2 +"]").parent().find("p").remove();
-                $("[name="+ name1 +"]").parent().find("p").remove();
+        if(val1 === val2 && txt1.length >= 8 && txt1.length <= 16){
+            if(eng.test(val1) && num.test(val1)){
+                const errMsg2 = txt2.closest("td").querySelector("p");
+                const errMsg1 = txt1.closest("td").querySelector("p");
+                if(errMsg2) errMsg2.remove();
+                if(errMsg1) errMsg1.remove();
                 return true;
             }else if(num.test(txt1) && spc.test(txt1)){
-                $("[name="+ name2 +"]").parent().find("p").remove();
-                $("[name="+ name1 +"]").parent().find("p").remove();
+                const errMsg2 = txt2.closest("td").querySelector("p");
+                const errMsg1 = txt1.closest("td").querySelector("p");
+                if(errMsg2) errMsg2.remove();
+                if(errMsg1) errMsg1.remove();
                 return true;
             }else if(eng.test(txt1) && spc.test(txt1)){
-                $("[name="+ name2 +"]").parent().find("p").remove();
-                $("[name="+ name1 +"]").parent().find("p").remove();
+                const errMsg2 = txt2.closest("td").querySelector("p");
+                const errMsg1 = txt1.closest("td").querySelector("p");
+                if(errMsg2) errMsg2.remove();
+                if(errMsg1) errMsg1.remove();
                 return true;
             }else{
-                $("[name="+ name2 +"]").parent().find("p").remove();
-                $("[name="+ name1 +"]").parent().find("p").remove();
-                $("[name="+ name1 +"]").parent().append(
-                    "<p class='caution'>영문 대소문자, 숫자, 또는 특수문자 중 2가지 이상 조합하여 8~16자로 입력해 주세요.</p>"
-                );
-                this.result.push($("label[for="+ name1 +"]").text());
+                const errMsg2 = txt2.closest("td").querySelector("p");
+                const errMsg1 = txt1.closest("td").querySelector("p");
+                if(errMsg2) errMsg2.remove();
+                if(errMsg1) errMsg1.remove();
+
+                const htmls = "<p class='caution'>영문 대소문자, 숫자, 또는 특수문자 중 2가지 이상 조합하여 8~16자로 입력해 주세요.</p>";
+                txt1.closest("td").innerHTML += htmls;
+                
+                this.result.push(document.querySelector(`label[for= ${name1}]`).innerText);
                 return false;
             }
         }else if(txt1 === txt2){
-            $("[name="+ name1 +"]").parent().find("p").remove();
-            $("[name="+ name2 +"]").parent().find("p").remove();
-            $("[name="+ name1 +"]").parent().append(
-                "<p class='caution'>영문 대소문자, 숫자, 또는 특수문자 중 2가지 이상 조합하여 8~16자로 입력해 주세요.</p>"
-            );
-            this.result.push($("label[for="+ name1 +"]").text());
+            const errMsg2 = txt2.closest("td").querySelector("p");
+            const errMsg1 = txt1.closest("td").querySelector("p");
+            if(errMsg2) errMsg2.remove();
+            if(errMsg1) errMsg1.remove();
+
+            let htmls = "<p class='caution'>영문 대소문자, 숫자, 또는 특수문자 중 2가지 이상 조합하여 8~16자로 입력해 주세요.</p>";
+            txt1.closest("td").innerHTML += htmls;
+            
+            this.result.push(document.querySelector(`label[for= ${name1}]`).innerText);
             return false;
         }else{
-            $("[name="+ name1 +"]").parent().find("p").remove();
-            $("[name="+ name2 +"]").parent().find("p").remove();
-            $("[name="+ name1 +"]").parent().append(
-                "<p class='caution'>영문 대소문자, 숫자, 또는 특수문자 중 2가지 이상 조합하여 8~16자로 입력해 주세요.</p>"
-            );
-            $("[name="+ name2 +"]").parent().append(
-                "<p class='caution'>동일한 비밀번호를 입력해 주세요.</p>"
-            );
-            this.result.push($("label[for="+ name1 +"]").text());
+            const errMsg2 = txt2.closest("td").querySelector("p");
+            const errMsg1 = txt1.closest("td").querySelector("p");
+            if(errMsg2) errMsg2.remove();
+            if(errMsg1) errMsg1.remove();
+
+            let htmls = "<p class='caution'>영문 대소문자, 숫자, 또는 특수문자 중 2가지 이상 조합하여 8~16자로 입력해 주세요.</p>";
+            let htmls2 = "<p class='caution'>동일한 비밀번호를 입력해 주세요.</p>";
+            txt1.closest("td").innerHTML += htmls;
+            txt2.closest("td").innerHTML += htmls2;
+            
+            this.result.push(document.querySelector(`label[for= ${name1}]`).innerText);
             return false;
         }
     }
     
     isSelect(name){
-        let txt = $("[name="+ name +"]").find("option:selected").val();
+        const sel = document.querySelector(`[name=${name}]`);
+        const sel_index = sel.options.selectedIndex;
+        const val = sel.options[sel_index].value;
         
-        if(txt !== ""){
-            $("[name="+ name +"]").parent().find("p").remove();
+        if(val !== ""){
+            const errMsg = sel.closest("td").querySelector("p");
+            if(errMsg) errMsg.remove();
             return true;
         }else{
-            $("[name="+ name +"]").parent().find("p").remove();
-            $("[name="+ name +"]").parent().append(
-                "<p class='caution'>항목을 선택해 주세요.</p>"
-            )
-            this.result.push($("label[for="+ name +"]").text());
+            const errMsg = sel.closest("td").querySelector("p");
+            if(errMsg) errMsg.remove();
+
+            const htmls = "<p class='caution'>항목을 선택해 주세요.</p>";
+            sel.closest("td").innerHTML += htmls;
+            
+            this.result.push(document.querySelector(`label[for=${name}]`).innerText);
+
             return false;
         }
     }
     
     isTxt(name){
-        let txt = $("[name="+ name +"]").val();
+        const txt = document.querySelector(`[name=${name}]`);
+        const val = txt.value;
         
-        if(txt !== ""){
-            $("[name="+ name +"]").parent().find("p").remove();
+        if(val !== ""){
+            const errMsg = txt.closest("td").querySelector("p");
+            if(errMsg) errMsg.remove();
             return true;
         }else{
-            $("[name="+ name +"]").parent().find("p").remove();
-            $("[name="+ name +"]").parent().append(
-                "<p class='caution'>필수 입력값을 입력해 주세요.</p>"
-            )
-            this.result.push($("label[for="+ name +"]").text());
+            const errMsg = txt.closest("td").querySelector("p");
+            if(errMsg) errMsg.remove();
+
+            const htmls = "<p class='caution'>필수 입력값을 입력해 주세요.</p>";
+            txt.closest("td").innerHTML += htmls;
+            
+            this.result.push(document.querySelector(`label[for=${name}]`).innerText);
             return false;
         }
     }
     
     isCheck(name, text){
-        let txt = $("[name="+ name +"]").is(":checked");
-        if(txt){
-            $("[name="+ name +"]").parent().find(".caution").remove();
+        const txt = document.querySelectorAll(`[name=${name}]`);
+        let isChecked = false;
+        for(let el of txt){
+            if(el.checked) isChecked = true;
+        }
+
+        if(isChecked){
+            const errMsg = txt[0].parentElement.querySelector(".caution");
+            if(errMsg) errMsg.remove();
+
             return true;
         }else{
-            $("[name="+ name +"]").parent().find(".caution").remove();
-            $("[name="+ name +"]").parent().append(
-                "<p class='caution'>"+ text +"에 체크해 주세요.</p>"
-                )
-                this.result.push(text);
+            const errMsg = txt[0].parentElement.querySelector(".caution");
+            if(errMsg) errMsg.remove();
+
+            const htmls = `<p class='caution'> ${text}에 체크해 주세요.</p>`;
+            txt[0].parentElement.innerHTML += htmls;
+
+            this.result.push(text);
             return false;
         }
     }
     
     isPhone(name1, name2, name3){
-        let txt2 = $("[name="+ name2 +"]").val();
-        let txt3 = $("[name="+ name3 +"]").val();
+        const num2 = document.querySelector(`[name= ${name2}]`);
+        const num3 = document.querySelector(`[name= ${name3}]`);
+        const val2 = num2.value;
+        const val3 = num3.value;
     
-        if(this.isSelect(name1) && txt2 !== "" && txt3 !== ""){
-            $("[name="+ name2 +"]").parent().find("p").remove();
-            $("[name="+ name2 +"]").parent().find("p").remove();
+        if(this.isSelect(name1) && val2.length >= 4 && val3.length >= 4){
+            const errMsg = num2.closest("td").querySelector("p");
+            if(errMsg) errMsg.remove();
             return true;
         }else{
-            $("[name="+ name2 +"]").parent().find("p").remove();
-            $("[name="+ name2 +"]").parent().append(
-                "<p class='caution'>필수 입력값을 입력해 주세요.</p>"
-            )
-            this.result.push($("label[for="+ name1 +"]").text());
+            const errMsg = num2.closest("td").querySelector("p");
+            if(errMsg) errMsg.remove();
+
+            const htmls = "<p class='caution'>필수 입력값을 입력해 주세요.</p>";
+            num2.closest("td").innerHTML += htmls;
+
+            this.result.push(document.querySelector(`label[for=${name1}]`).innerText);
             return false;
         }
     }
     
     isAddress(name1, name2, name3){
-        let txt1 = $("[name="+ name1 +"]").val();
-        let txt2 = $("[name="+ name2 +"]").val();
-        let txt3 = $("[name="+ name3 +"]").val();
+        const txt1 = document.querySelector(`[name=${name1}]`);
+        const txt2 = document.querySelector(`[name=${name2}]`);
+        const txt3 = document.querySelector(`[name=${name3}]`);
+        const val1 = txt1.value;
+        const val2 = txt2.value;
+        const val3 = txt3.value;
         
-        if(txt1 !== "" && txt2 !== "" && txt3 !==""){
-            $("[name="+ name1 +"]").parent().find("p").remove();
+        if(val1 !== "" && val2 !== "" && val3 !==""){
+            const errMsg = txt1.closest("td").querySelector("p");
+            if(errMsg) errMsg.remove();
             return true;
         }else{
-            $("[name="+ name1 +"]").parent().find("p").remove();
-            $("[name="+ name1 +"]").parent().append(
-                "<p class='caution'>필수 입력값을 입력해 주세요.</p>"
-            )
-            this.result.push($("label[for="+ name1 +"]").text());
+            const errMsg = txt1.closest("td").querySelector("p");
+            if(errMsg) errMsg.remove();
+
+            const htmls = "<p class='caution'>필수 입력값을 입력해 주세요.</p>";
+            txt1.closest("td").innerHTML += htmls;
+            
+            this.result.push(document.querySelector(`label[for=${name1}]`).innerText);
             return false;
         }
     }
     
     isEmail(name1, name2, name3){
-        let txt1 = $("[name="+ name1 +"]").val();
-        let txt2 = $("[name="+ name2 +"]").val();
-        let txt3 = this.isSelect(name3);
+        const txt1 = document.querySelector(`[name=${name1}]`);
+        const txt2 = document.querySelector(`[name=${name2}]`);
+        const isChecked = this.isSelect(name3);
+        const val1 = txt1.value;
+        const val2 = txt2.value;
         
-        if(txt1 !== "" && txt2 !== "" || txt3){
-            $("[name="+ name1 +"]").parent().find("p").remove();
+        console.log(txt1);
+        console.log(txt1.closest("td"));
+
+        if(val1 !== "" && (val2 !== "" || isChecked)){
+            const errMsg = txt1.closest("td").querySelector("p");
+            if(errMsg) errMsg.remove();
             return true;
         }else{
-            $("[name="+ name1 +"]").parent().find("p").remove();
-            $("[name="+ name1 +"]").parent().append(
-                "<p class='caution'>필수 입력값을 입력해 주세요.</p>"
-            )
-            this.result.push($("label[for="+ name1 +"]").text());
+            const errMsg = txt1.closest("td").querySelector("p");
+            if(errMsg) errMsg.remove();
+
+            const htmls = "<p class='caution'>필수 입력값을 입력해 주세요.</p>"
+            txt1.closest("td").innerHTML += htmls;
+            
+            this.result.push(document.querySelector(`label[for=${name3}]`).innerText);
             return false;
         }
     }    
