3c4638d1022ae33767df9b38b7cd152854b56968
suyeon-hong <bellahong95@gmail.com>
2021-12-20 20:09:46 +0900

    commity tabs 버튼 필터링기능 추가

--- js/community.js
+++ js/community.js
@@ -1,6 +1,8 @@
+const tab = document.querySelector(".community .tabs");
 const tabs = document.querySelectorAll(".community .tabs li");
 const faq_nums = document.querySelectorAll(".community #faq .numbers span");
 const notice_nums = document.querySelectorAll(".community #notice .numbers span");
+const qna = document.querySelector(".qna");
 
 activeBtn(tabs);
 activeBtn(faq_nums);
@@ -17,12 +19,22 @@ function activeBtn(items){
     });
 }
 
-const qna = document.querySelector(".qna");
 
 fetch('../data.json')
-.then(data=> {return data.json()})
+.then(data=> data.json())
 .then(json=> {
     const items = json.qna;
+    createList(items);
+    tab.addEventListener("click", e=>{
+        const value = e.target.dataset.value;
+    
+        if(value == null) return;
+        updateItems(items, value);
+    });
+});
+
+
+function createList(items){
     let htmls = "";
 
     items.map(item=>{
@@ -40,4 +52,27 @@ fetch('../data.json')
         `;
     });
     qna.innerHTML = htmls;
-});
+}
+
+function updateItems(items, value){
+    let htmls = "";
+
+    let result = items.filter(item => item["subject"] == value);
+    if (value == "전체") result = items;
+
+    result.map(item=>{
+        htmls += `
+            <dt>
+                <span class="icon">Q</span>
+                <p>${item.question}</p>
+                <span class="subj">${item.subject}</span>
+                <a href="#" class="arrow" title="답변보기"></a>
+            </dt>
+            <dd>
+                <span class="icon">A</span>
+                <p>${item.answer}</p>
+            </dd>
+        `;
+    });
+    qna.innerHTML = htmls;
+}
